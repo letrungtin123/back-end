fbed64c991fa0d3acd00a5be118d4231b9e2a902
letrungtin123 <lttvn917@gmail.com>
2024-06-01 17:36:45 +0700

    thêm

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 import cateoryRoutes from './routes/category.route.js';
 import connectDb from './configs/connect-db.config.js';
 import express from 'express';
+import productRoutes from './routes/product.route.js'
 
 dotenv.config();
 
@@ -13,7 +14,7 @@ app.use(express.json());
 connectDb();
 
 app.use('/api/v1', cateoryRoutes);
-
+app.use('/api/v1',productRoutes )
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
